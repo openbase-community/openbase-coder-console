fcb63b49e96a79c92f836ec0401f57a35f6294de
Gabriel Montague <montaguegabe@users.noreply.github.com>
2026-07-03 21:26:01 -0400

    Trim console package to host shell
    Agent-Thread-Id: 019f2ab9-eb13-74c3-adac-ea485eff9cc2

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "@openbase/coder-react/App";
 import { PluginRegistryProvider } from "@openbase/coder-react/plugin-registry";
 import "@openbase/coder-react/index.css";
-import { pluginConsolePages, pluginProjectViews } from "./pluginLoader";
-
-const originalError = console.error;
-console.error = (...args) => {
-  // TODO: Pass error to Openbase
-  originalError(...args); // Pass through all other errors
-};
 
 createRoot(document.getElementById("root")!).render(
-  <PluginRegistryProvider
-    pluginConsolePages={pluginConsolePages}
-    pluginProjectViews={pluginProjectViews}
-  >
+  <PluginRegistryProvider>
     <App />
   </PluginRegistryProvider>
 );
