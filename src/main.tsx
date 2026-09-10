45d73df2c0e765bc5216025d8fe5a04658e65c01
Gabriel Montague <montaguegabe@users.noreply.github.com>
2026-09-10 19:11:11 -0400

    Enable shared appearance preferences in the console
    Agent-Thread-Id: 01a08c71-61b1-7253-bbd8-9fce686f7b9c

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "@openbase/coder-react/App";
+import { AppearanceProvider } from "@openbase/coder-react/appearance";
 import { PluginRegistryProvider } from "@openbase/coder-react/plugin-registry";
 import "@openbase/coder-react/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <PluginRegistryProvider>
-    <App />
-  </PluginRegistryProvider>
+  <AppearanceProvider>
+    <PluginRegistryProvider>
+      <App />
+    </PluginRegistryProvider>
+  </AppearanceProvider>
 );
